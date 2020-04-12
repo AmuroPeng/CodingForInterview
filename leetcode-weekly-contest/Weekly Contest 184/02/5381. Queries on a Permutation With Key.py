eec0597e92638b3306c2b9b5bbca5dafb2ffe018
AmuroPeng <tiggerpeng1997@gmail.com>
2020-04-12 00:32:49 -0400

    Update 5381. Queries on a Permutation With Key.py

--- leetcode-weekly-contest/Weekly Contest 184/02/5381. Queries on a Permutation With Key.py
+++ leetcode-weekly-contest/Weekly Contest 184/02/5381. Queries on a Permutation With Key.py
@@ -1,22 +1,14 @@
 class Solution:
     def processQueries(self, queries: list, m: int) -> list:
-        P = []
-        list_=[]
-        for i in range(m):
-            P.append(i+1)
+        P = list(i+1 for i in range(m))
+        list_ = []
 
         for i in queries:
             move = P.index(i)
             list_.append(move)
-            left = P[:move]
-            # if move == 0:
-            #     left =[]
-            right = P[move+1:]
-            # if move == len(P):
-            #     right =[]
-            P = [P[move]]+list(left)+list(right)
-            # print(P)
+            P = [P[move]]+P[:move]+P[move+1:]
         return list_
+
 
 if __name__ == "__main__":
     s = Solution()
