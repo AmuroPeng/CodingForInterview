4e360bc9f3d03f2210d334a3a69d6f6dfe6c1a36
AmuroPeng <tiggerpeng1997@gmail.com>
2020-05-19 11:36:38 -0400

    19

---
+++ 2020May-30-day-leetcoding-challenge/19-Online Stock Span/Online Stock Span.py
@@ -0,0 +1,18 @@
+class StockSpanner:
+
+    def __init__(self):
+        pass
+
+    def next(self, price: int) -> int:
+        pass
+
+
+# Your StockSpanner object will be instantiated and called as such:
+# obj = StockSpanner()
+# param_1 = obj.next(price)
+
+if __name__ == "__main__":
+    pass
+    s=StockSpanner()
+    s1=s.next(100)
+    
