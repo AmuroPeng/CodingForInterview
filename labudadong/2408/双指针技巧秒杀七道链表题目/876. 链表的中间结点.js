8c5fe28706a0e532273a4c75bcff8a27d6bdd42a
AmuroPeng <tiggerpeng1997@gmail.com>
2024-09-13 01:34:21 +0800

    coding

---
+++ labudadong/2408/双指针技巧秒杀七道链表题目/876. 链表的中间结点.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function(head) {
+  let p1 = head;
+  let p2 = head;
+  while (p1?.next) {
+      p1 = p1.next;
+      p2 = p2.next;
+      if (p1) p1 = p1.next;
+  }
+  return p2;
+};
