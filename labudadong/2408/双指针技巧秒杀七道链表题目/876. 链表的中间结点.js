49230e64798659c9940704d355962243957a8759
Amuro Peng <tiggerpeng1997@gmail.com>
2025-01-06 17:08:54 +0000

    876. 链表的中间结点

--- labudadong/2408/双指针技巧秒杀七道链表题目/876. 链表的中间结点.js
+++ labudadong/2408/双指针技巧秒杀七道链表题目/876. 链表的中间结点.js
@@ -19,3 +19,24 @@ var middleNode = function(head) {
   }
   return p2;
 };
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function(head) {
+  let l = head;
+  let r = head;
+  while (r && r.next) {
+      l = l.next;
+      r = r.next.next;
+  }
+  return l;
+};
