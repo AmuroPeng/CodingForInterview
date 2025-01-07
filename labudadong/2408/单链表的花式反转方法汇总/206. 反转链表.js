0a154a236733c5a08b329c15b69751900a785b5b
Amuro Peng <tiggerpeng1997@gmail.com>
2025-01-07 17:31:01 +0000

    206. 反转链表

---
+++ labudadong/2408/单链表的花式反转方法汇总/206. 反转链表.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    // 1. 迭代
+    // if (head === null || head.next === null) return head;
+    // let pre, cur, nxt;
+    // pre = null;
+    // cur = head;
+    // nxt = head.next;
+    // while (cur !== null) {
+    //     cur.next = pre;
+    //     pre = cur;
+    //     cur = nxt;
+    //     if (nxt) nxt = nxt.next;
+    // }
+    // return pre;
+};
